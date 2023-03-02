6e4e833da0aba5d01fd399585f4705b8f65454d0
Shailendra <kumarshai@gmail.com>
2023-03-02 14:30:02 -0600

    First commit 14

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+    return 'Web App with Python Flask from dockerStrategy!'
 
 @app.route('/test')
 def test():
-    return 'THis is testing Shailendra!'
+    return 'THis is dockerStrategy testing Shailendra!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
